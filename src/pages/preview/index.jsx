d4a637e05f19cd119f17848fe139ba3be7541000
Ruthie Boat <ruthboatengs1@gmail.com>
2024-07-11 09:39:50 +0000

    preview commit

---
+++ src/pages/preview/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Preview = () => {
+  return (
+    <div>
+      <div>
+        <h1>Ruth BOATENG</h1>
+        <p>Frontend Dev, UI/UX designer, Graphic Designer and Copywriter3</p>
+        <button>DOWNLOAD CV</button>
+      </div>
+      <div>
+        <h1>ABOUT</h1>
+        <p>Hi I'm Jackson Ford On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
+      </div>
+      <div>
+        <h1>
+        I am happy to know you
+        that 300+ projects done sucessfully!
+        </h1>
+        <button>HIRE ME</button>
+      </div>
+      <div>
+        <h1>HERE ARE SOME OF MY EXPERTISE</h1>
+        <p></p>
+      </div>
+      <span>
+        <p>MY SKILLS</p>
+      </span>
+    </div>
+  )
+}
+
+export default Preview

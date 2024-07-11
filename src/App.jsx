6e699bc4a926e9492b25f3094271b4dd353d27df
EyramAnn <anndekonor@gmail.com>
2024-07-11 11:31:24 +0000

    update the dashbord UI interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DashboardLayout from "./pages/dashboard/layouts";
+import Overview from "./pages/dashboard/pages/overview";
+import Landing from "./pages/landing";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
+import Home from "./pages/dashboard/pages/home";
+import Skills from "./pages/dashboard/pages/skills";
+import Services from "./pages/dashboard/pages/services";
+import Education from "./pages/dashboard/pages/education";
+import Experience from "./pages/dashboard/pages/experience";
+import Work from "./pages/dashboard/pages/work";
+import Blog from "./pages/dashboard/pages/blog";
+import Contact from "./pages/dashboard/pages/contact";
+import Preview from "./pages/preview";
 
 function App() {
-  const router = createBrowserRouter ([
+  const router = createBrowserRouter([
     {
       path: "/",
-      element: "<Landing />",
+      element: <Landing />,
     },
     {
       path: "login",
-      element: "<Login />",
+      element: <Login />,
     },
     {
       path: "signup",
-      element: "<SignUp />",
+      element: <SignUp />,
     },
     {
       path: "dashboard",
-      element: "<Dashboard />",
+      element: <DashboardLayout />,
+      children: [
+        {
+          index: true,
+          element: <Overview />,
+        },
+        {
+          path: "home",
+          element: <Home />,
+        },
+        {
+          path: "skills",
+          element: <Skills />,
+        },
+        {
+          path: "services",
+          element: <Services />,
+        },
+        {
+          path: "education",
+          element: <Education />,
+        },
+        {
+          path: "experience",
+          element: <Experience />,
+        },
+        {
+          path: "work",
+          element: <Work />,
+        },
+        {
+          path: "blog",
+          element: <Blog />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+      ],
     },
     {
       path: "preview",
-      element: "<Preview />",
-    }
+      element: <Preview />,
+    },
   ]);
 
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/dashboard/layouts/pagesLayout.jsx
@@ -0,0 +1,16 @@
+import { PlusCircleIcon } from "lucide-react";
+
+const PagesLayout = ({ headerText, buttonText, children }) => {
+  return (
+    <div className="p-10 flex-col gap-y-16">
+      <div className="flex ">
+        <h1 className="text-2xl font-bold">{headerText}</h1>
+        <button className="bg-[#0A524F] text-white px-6 py-2 ml-auto rounded-lg flex-gap-x-2 ">
+          <PlusCircleIcon /> {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PagesLayout;

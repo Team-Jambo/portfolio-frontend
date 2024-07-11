d14b55676e4bcbbc1dd2fa5b311b78f5f93fd1b4
Ruthie Boat <ruthboatengs1@gmail.com>
2024-07-11 15:27:03 +0000

    Building Login page

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import Login from "../../pages/auth/Login";
 
 const LoginForm = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-        <form action="">
-            <h1>Login</h1>
-            <div>
-                <input type="text" placeholder=''/>
-            </div>
-        </form>
-    </div>
+
+    <form className="" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <div>
+          <label
+            htmlFor="name"
+            className="font-semibold">
+            Username
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Enter your Email Address"
+            className="border-2"
+            {...register("name", {
+              required: "Email Address is required", minLength: 2,
+            })}
+          />
+          {errors.name && (
+            <p className="text-red-500">{errors.name.message}</p>)}
+        </div>
+
+        <div>
+          <label
+            htmlFor="passname"
+            className="font-semibold">
+            Password
+          </label>
+          <input
+            id="passname"
+            type="text"
+            placeholder="Enter your password"
+            className="border-2"
+            {...register("name", {
+              required: "Password is required", minLength: 2,
+            })}
+          />
+          {errors.name && (
+            <p className="text-red-500">{errors.passname.message}</p>)}
+        </div>
+
+      </div>
+    </form>
+
   )
 }
 
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
+import { FacebookIcon, GithubIcon, InstagramIcon, LinkedinIcon } from 'lucide-react'
+import LoginForm from '../../components/LoginForm/LoginForm'
 
 const Login = () => {
   return (
-    <div>
-      <form >
-      <div className="mb-8 pt-14">
-          <label
-            htmlFor="name"
-            className="block text-black font-medium mb-1 ml-4"
-          ></label>
-          <input
-            type="text"
-            placeholder="Email Address"
-            className="h-10 w-full px-2 py-1 outline-transparent border-[#2d6599] border-2 rounded-lg "
-          />
-           <label
-            htmlFor="name"
-            className="block text-black font-medium mb-1 ml-4"
-          ></label>
-          <input
-            type="text"
-            placeholder="Password"
-            className="h-10 w-full px-2 py-1 outline-transparent border-[#2d6599] border-2 rounded-lg "
-          />
+    <div className='flex justify-center p-10 bg-violet-300 +items-center'>
+      <div className='flex flex-col justify-center items-center bg-violet-700 w-[50%] h-[50%] rounded-2xl shadow-2xl'>
+        <img src="" alt="" />
+        <div className='flex flex-col gap-4 items-center p-3'>
+          <h1 className='text-white font-bold text-3xl pt-3'>Login to Your Account</h1>
+          <p className=' flex font-semibold '>Login using your social networks</p>
         </div>
-      </form>
+        <div className='flex flex-row gap-5'>
+          <FacebookIcon className=' bg-white rounded-full size-12 text-gray-800 text-sm' />
+          <InstagramIcon className=' bg-white rounded-full size-12 text-gray-800' />
+          <GithubIcon className=' bg-white rounded-full size-12 text-gray-800' />
+          <LinkedinIcon className=' bg-white rounded-full size-12 text-gray-800' />
+        </div>
+        <div className='flex w-1/2 pt-20'>
+          <hr className='w-[45%]' />
+          <span>OR</span>
+          <hr className='w-[45%]' />
+        </div>
+        <form >
+          <div className=" mb-8 pt-14 ">
+            <label
+              htmlFor="name"
+              className="block text-white font-medium mb-1 ml-4"
+            ></label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              className="h-10 w-[400px] px-2 py-1 outline-transparent border-violet-950 border-2 rounded-3xl"
+            />
+            <label
+              htmlFor="name"
+              className="block text-white font-medium mb-1 ml-4"
+            ></label>
+            <input
+              type="text"
+              placeholder="Enter Password"
+              className="h-10 w-[400px] px-2 py-1 outline-transparent border-violet-950 border-2 rounded-3xl "
+            />
+
+            <div className='flex justify-center p-2'>
+              <button className=' bg-white rounded-2xl h-8 w-32 border-violet font-semibold '>Sign In</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <LoginForm />
     </div>
   )
 }

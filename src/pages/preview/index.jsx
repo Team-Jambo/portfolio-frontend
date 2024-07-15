a3a78593fbed65690e3f98d81ccc83100ec594ec
Ruthie Boat <ruthboatengs1@gmail.com>
2024-07-15 01:41:27 +0000

    penultimate commit for preview

--- src/pages/preview/index.jsx
+++ src/pages/preview/index.jsx
@@ -38,6 +38,9 @@ const Preview = () => {
         </span>
         <span>
           <p>Hi I'm Jackson Ford On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
+          <div>
+            <input type="text" />
+          </div>
         </span>
         <span>
           <h1>MY SERVICES</h1>

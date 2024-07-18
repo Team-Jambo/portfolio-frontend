8618b5ce7768f5294f28496ff9b7360d49b824e4
Ruthie Boat <ruthboatengs1@gmail.com>
2024-07-18 18:34:51 +0000

    login commit

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,10 @@
 import { set, useForm } from "react-hook-form";
-import log from "../../assets/images/newone.jpg"
+import log from "../../assets/images/picture.jpeg"
 import { apiLogin } from "../../services/auth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Loader from "../../components/Loader";
+import { toast } from "react-toastify";
 
 const Login = () => {
 
@@ -10,7 +12,6 @@ const Login = () => {
   console.log(isSubmitting);
 
   const navigate = useNavigate()
-
 
   const { register, 
     handleSubmit, 
@@ -27,12 +28,16 @@ const Login = () => {
         password: data.password
       })
       console.log("Response", res.data);
-      // redirect user to dashboard
-      navigate("/dashboard")
+      localStorage.setItem("accessToken", res.data.accessToken);
 
+      toast.success(res.data.message)
+      setTimeout(() => {
+        navigate("/dashboard")
+      }, 5000)
 
     } catch (error) {
       console.log(error)
+      toast.error("An error occured")
     }
     finally {
       setIsSubmitting(false)
@@ -41,15 +46,16 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-row justify-center bg-white rounded-lg pt-20">
-      <div className="w-50% relative mr-10 ">
-        <img src={log} alt="login image" className="w-full" />
+    <div className="flex flex-row justify-between bg-white rounded-lg gap-x-5">
+      <div className="w-50% relative pt-10 ">
+        <img src={log} alt="login image" />
       </div>
 
       <div className="flex flex-col rounded-lg">
-        <div className="w-50%">
-          <h1 className="flex justify-around text-3xl font-bold p-10"> Welcome back</h1>
-          <p className="flex justify-around text-sm font-semibold  text-[#0B4459]">Login with your personal information </p>
+
+        <div className="flex w-50% ">
+          <h1 className="flex  text-3xl font-bold p-10"> Welcome back</h1>
+          <p className="flex text-sm font-semibold  text-[#0B4459]">Login with your personal information </p>
         </div>
         <form
           className="flex flex-col max-w-md mx-auto pt-5"
@@ -86,7 +92,7 @@ const Login = () => {
               className="h-9 w-[450px] px-2 py-1 outline-transparent bg-white border-gray border-2"
               {...register("password", {
                 required: "password is required",
-                minLength: 8,
+                minLength: 5,
               })}
             />
             {errors.password && (
@@ -98,7 +104,7 @@ const Login = () => {
             type="submit"
             className="bg-[#0B4459] text-white w-full py-1 rounded-md font-semibold"
           >
-            {isSubmitting ? "Loading..." : "Login"}
+            {isSubmitting ? <Loader /> : "Login"}
           </button>
           <div className="flex flex-row text-sm font-semibold justify-evenly pt-4">
             <p>Don't Have An Account?</p>
